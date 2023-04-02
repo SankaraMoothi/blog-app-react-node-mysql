7ae30bad1a0e100db81e0da74edae0b35984b4c1
SankaraMoothi <sankaramoorthi99@gmail.com>
2023-04-02 22:48:55 +0530

    firstBug

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -50,7 +50,7 @@ const Home = () => {
   return (
     <div className="home">
       <div className="posts">
-        {posts.map((post) => (
+        {posts?.map((post) => (
           <div className="post" key={post.id}>
             <div className="img">
               <img src={`../upload/${post.img}`} alt="" />
